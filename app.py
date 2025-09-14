9ae8efa27ff8aca43a477daa7213eaf4a6db1ffd
Steven Chen <59878883+wannabeoysterfarmer@users.noreply.github.com>
2025-09-14 13:23:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -208,7 +208,7 @@ def process_pdf_and_generate_deck(
 # ---------------- UI ----------------
 st.set_page_config(page_title="Decksmith", page_icon="⚔️")
 st.title("Introducing: Decksmith ⚒️ ")
-st.title("Your all in one PDF → Anki Deck Generator")
+st.title("Your all in one PDF → Anki Deck Generator 🔥🔥🔥")
 st.markdown("""
 ### How to Use Decksmith
 
